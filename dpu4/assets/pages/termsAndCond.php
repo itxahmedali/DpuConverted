d8146b643c400305e9a80eea5f5d29c7d78d0a0c
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2022-07-27 17:46:07 +0500

    add new pages

---
+++ dpu4/assets/pages/termsAndCond.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include '../Commons/Head.php' ?>
+
+<body>
+    <?php include '../Commons/Navbar.php' ?>
+    <!-- ==============BUTTER JS =================== -->
+    <div id="butter">
+        <div class="aboutuspage terms">
+            <!-- banner -->
+            <div class="banner wow fadeIn" data-wow-delay="0.1s" data-wow-duration="0.5s">
+                <h1 class="wow bounceInDown" data-wow-delay="0.2s" data-wow-duration="1.0s">
+                    Terms And Conditions
+                </h1>
+            </div>
+            <div class="container bannerText mx-auto wow bounceInUp" data-wow-delay="0.5s" data-wow-duration="1.0s">
+                <p class="pt-5">
+                    By using Design Pros USA website, the user hereby agrees to fully comply with this stated set of use of website terms and conditions. If in any case the user does not solely agree and oblige with the stated terms and conditions then one may not use Design Pros USA in any of the ways.
+
+                    This statement of terms and conditions hereby ratifies that Design Pros USA, has complete rights and autonomy to revise, change, update or edit this statement of terms and conditions and being a user of Design Pros USA it is mandatory to fully comply with these terms of use as the basic set of mutual understanding and consent to use Design Pros USA.
+
+                    For all the stated terms and conditions Design Pros USA have a zero level tolerance and both the parties Design Pros USA and the User are required to follow and fully comply with these stated terms and conditions respectively. All the activities are carried out by Design Pros USA on the mutual understanding that the user has entirely agreed with the use of Design Pros USA website terms and conditions.
+
+                    The logo, Name and Graphics of Design Pros USA and its products and services are the trademarks of Design Pros USA. All other company names, brand names, trademarks and logos mentioned on the website are the property of their respective owners and do not constitute or imply endorsement, sponsorship or recommendation thereof by Design Pros USA and do not constitute or imply endorsement, sponsorship or recommendation of Design Pros USA at all by the respective trademark owners.
+                </p>
+
+                <span class="h-1">INDEMNITY</span>
+                <p>
+                    Being a user of this website You agree to defend, indemnify and hold harmless Design Pros USA and the directors, officers, managers, employees and representatives of each of them, for any type of loss, damages or costs, including reasonable lawyers’ fees, resulting from any third party claim, action, or demand related to your name or the use thereof.
+
+                    For any third party integrations (Plugins/APIs), Design Pros USA will be reponsible for integration of those softwares. You will not be holding Design Pros USA for payments (if any) for those softwares. You will be purchasing any third party APIs/Plugins and give access to Design Pros USA for integration.
+
+                    For Design Pros USA, if a user shares one’s website in regard of project proposal or works activity, all the elements on your website is considered a publication of yours and Design Pros USA is not considered to be held responsible for anything you present on the web through your domain at all.
+
+                    All of your logos, Names, Graphics, Design Theme, Brand Names, Trademarks, the Content, Words, Pictures, Images, Ideas, Visuals and Illustrations are all your assets and Design Pros USA will not claim the ownership in any way.
+
+
+                </p>
+                <span class="h-1">REFUSAL</span>
+                <p>
+                    Design Pros USA shall be the sole arbitrator of what is and is not a violation of these acceptable use terms and conditions. Design Pros USA reserves complete rights to update or revise this statement anytime. Design Pros USA reserves the right to refuse, cancel, or suspend service at our sole discretion as well.
+
+                    In cases that any provision of this Agreement of use of website terms and conditions is deemed invalid, such invalidity shall not affect the remainder of this Agreement on terms and conditions by any means. Design Pros USA have complete rights and autonomy that it may amend or replace such invalid provision with one that is completely valid and which achieves, to the extent possible, the original objectives and intent of Design Pros USA as reflected in the respective original provision.
+
+                    Design Pros USA reserves absolute right to change the use of website terms and conditions without any prior notice or warning. Design Pros USA highly recommend its users to review and read this statement of use of website terms and conditions in a timely manner in order to stay up to date with the amendments if made.
+
+                    This statement of agreement supersedes and governs all prior proposals, agreements, or other communications made to the users of Design Pros USA’ website.
+                </p>
+                <span class="h-1">COPYRIGHTS AND TRADEMARKS</span>
+                <p>
+                    By supplying logos, names, graphics, design theme, brand names, trademarks, content, words, pictures, images, ideas, visuals, illustrations, and other data to Design Pros USA for inclusion in the customer’s website or other medium, the customer hereby fully declares that such material holds the appropriate copyright and/or trademark permissions. The ownership of such materials will remain with the customer.
+
+                    Any creative work or artwork, images, or content supplied and/or designed by Design Pros USA on behalf of the customer, will remain the property of the customer.
+
+                    The users agree to fully indemnify and hold Design Pros USA free from all types of harm in any and all claims resulting from the customer in first not having obtained all the required copyright, and/or any other necessary permissions or grants.
+
+                </p>
+
+                <span class="h-1">CANCELLATION
+                </span>
+                <p>
+                    After design approval from the client,the signed contracts cannot be terminated since the client has given their consent on the digital assets.
+
+
+                    In case of project cancellation after the project commencement from the client before design approval. A total of 30% of the total project budget will be deducted and the remaining shall be refunded to the client after due process.
+
+
+                    Customer will pay Design Pros USA the Recurring Monthly Fees (if applicable) as specified in the contract signed OR order placed with Design Pros USA. The customer shall provide a minimum 30 days or greater written notice prior to the end of the Term of such services, Design Pros USA may charge any fees payable under this Agreement.
+
+                </p>
+
+            </div>
+
+
+
+
+
+
+        </div>
+        <!-- contact page -->
+        <section class="nine-section img-div nomove6 position-relative">
+            <div class="container nomovo">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="contact-heading mt2">
+                            <h1>
+                                WELL, <br />
+                                ENOUGH <br />
+                                CHITCHAT. <br />
+                                LET’S GET TO <br />
+                                WORK! <br />
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="contact-form">
+                            <input type="text" placeholder="Full Name" />
+                            <input type="email" placeholder="Email" />
+                            <input type="text" placeholder="Country" />
+                            <input type="text" placeholder="Contact No." />
+                            <!-- <input type="text" placeholder="Service"> -->
+                            <select name="cars" id="cars" aria-placeholder="Service">
+                                <option selected disabled value="Service">Service</option>
+                                <option value="Website Development">
+                                    Website Development
+                                </option>
+                                <option value="App Development">App Development</option>
+                                <option value="Animation and Designing">
+                                    Animation and Designing
+                                </option>
+                                <option value="Digital Marketing">Digital Marketing</option>
+                            </select>
+                            <textarea name="" placeholder="Message" id="" cols="30" rows="10"></textarea>
+                        </div>
+                        <div>
+                            <a href=""><button class="main-transparnet sendBtn">SEND</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header">
+                <div id="containerCST">
+                    <canvas id="canvas"> </canvas>
+                </div>
+                <svg width="100%" height="55%" version="1.1" xmlns="http://www.w3.org/2000/svg" class="wave">
+                    <defs></defs>
+                    <path id="feel-the-wave" d="" />
+                </svg>
+                <svg width="100%" height="55%" version="1.1" xmlns="http://www.w3.org/2000/svg" class="wave">
+                    <defs></defs>
+                    <path id="feel-the-wave-two" d="" />
+                </svg>
+            </div>
+        </section>
+        <!--======================================= Footer ==========================================-->
+        <?php include '../Commons/Footer.php' ?>
+    </div>
+
+    <section class="video-inner-modal-section">
+        <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg position-relative">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <div class="button-position">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <video controls width="100%">
+                            <source src="../video/file.mp4" type="video/mp4" />
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="videoModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <video controls width="100%">
+                            <source src="../video/file.mp4" type="video/mp4" />
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="videoModal3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-lg">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <video controls width="100%">
+                            <source src="../video/file.mp4" type="video/mp4" />
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include '../Commons/Scripts.php' ?>
+</body>
+
+</html>
